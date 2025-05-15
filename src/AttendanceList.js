803df87f5aa7a23658eebe01dbbdf1b9a320fcbc
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-05-15 15:26:51 +0530

    Update AttendanceList.js

--- src/AttendanceList.js
+++ src/AttendanceList.js
@@ -1,58 +1,51 @@
-import react, * as React from "react";
+import * as React from "react";
 import { Page, Grid, Table, Button } from "tabler-react";
 import SiteWrapper from "./SiteWrapper.react";
-//import { withTransaction } from '@elastic/apm-rum-react';
 
 class ListEmployee extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = { data: [] }
-	}
-	
-	loadData() {
-		fetch('/attendance/search')
-			.then(response => response.json())
-			.then(data => {
-				this.setState({data: data })
-		})
-			.catch(err => console.error(this.props.url, err.toString()))
-	}
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
 
-	componentDidMount() {
-		this.loadData()
-	}
-	
+  loadData() {
+    fetch("/attendance/search")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ data: data });
+      })
+      .catch((err) => console.error("Error fetching data:", err));
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
   render() {
-      return (
-          <SiteWrapper>
-          <Page.Card
-              title="Attendance List"
-          ></Page.Card>
-          <Grid.Col md={6} lg={10} className="align-self-center">
+    return (
+      <SiteWrapper>
+        <Page.Card title="Attendance List"></Page.Card>
+        <Grid.Col md={6} lg={10} className="align-self-center">
           <Table>
             <Table.Header>
-                 <Table.ColHeader>Employee ID</Table.ColHeader>
-                 <Table.ColHeader>Status</Table.ColHeader>
-                 <Table.ColHeader>Date</Table.ColHeader>
+              <Table.ColHeader>Employee ID</Table.ColHeader>
+              <Table.ColHeader>Status</Table.ColHeader>
+              <Table.ColHeader>Date</Table.ColHeader>
             </Table.Header>
             <Table.Body>
-           { this.state.data.map((item, i) => {
-                return (
-                    <Table.Row>
-                        <Table.Col>{item.id}</Table.Col>
-                        <Table.Col>{item.status}</Table.Col>
-                        <Table.Col>{item.date}</Table.Col>
-                    </Table.Row>  
-                );
-                })  
-            }
+              {this.state.data.map((item, i) => (
+                <Table.Row key={i}>
+                  <Table.Col>{item.id}</Table.Col>
+                  <Table.Col>{item.status}</Table.Col>
+                  <Table.Col>{item.date}</Table.Col>
+                </Table.Row>
+              ))}
             </Table.Body>
-            </Table>
-          </Grid.Col>
-          </SiteWrapper>
-      );
+          </Table>
+        </Grid.Col>
+      </SiteWrapper>
+    );
   }
 }
 
-export default ListEmployee
-//export default withTransaction('ListEmployee', 'component')(ListEmployee)
+export default ListEmployee;
