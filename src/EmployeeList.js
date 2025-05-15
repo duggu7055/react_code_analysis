baaf515a6ea6b8048e542a9873e60f5762dda442
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-05-15 15:40:15 +0530

    Update EmployeeList.js

--- src/EmployeeList.js
+++ src/EmployeeList.js
@@ -1,64 +1,76 @@
-import react, * as React from "react";
-import { Page, Grid, Table, Button } from "tabler-react";
+import React from "react";
+import { Page, Grid, Table } from "tabler-react";
 import SiteWrapper from "./SiteWrapper.react";
-//import { withTransaction } from '@elastic/apm-rum-react';
 
 class ListEmployee extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = { data: [] }
-	}
-	
-	loadData() {
-		fetch("/employee/search/all")
-			.then(response => response.json())
-			.then(data => {
-				this.setState({data: data })
-		})
-			.catch(err => console.error(this.props.url, err.toString()))
-	}
+  constructor(props) {
+    super(props);
+    this.state = { data: [], loading: true, error: null };
+  }
 
-	componentDidMount() {
-		this.loadData()
-	}
-	
-  render() {
-      return (
-          <SiteWrapper>
-          <Page.Card
-              title="Employee List"
-          ></Page.Card>
-          <Grid.Col md={6} lg={10} className="align-self-center">
-          <Table>
-            <Table.Header>
-                 <Table.ColHeader>Employee ID</Table.ColHeader>
-                 <Table.ColHeader>Name</Table.ColHeader>
-                 <Table.ColHeader>Email</Table.ColHeader>
-                 <Table.ColHeader>Phone Number</Table.ColHeader>
-                 <Table.ColHeader>Job Role</Table.ColHeader>
-                 <Table.ColHeader>Job Location</Table.ColHeader>
-            </Table.Header>
-            <Table.Body>
-           { this.state.data.map((item, i) => {
-                return (
-                    <Table.Row>
-                        <Table.Col>{item.id}</Table.Col>
-                        <Table.Col>{item.name}</Table.Col>
-                        <Table.Col>{item.email}</Table.Col>
-                        <Table.Col>{item.phone_number}</Table.Col>
-                        <Table.Col>{item.job_role}</Table.Col>
-                        <Table.Col>{item.location}</Table.Col>
-                    </Table.Row>  
-                );
-                })  
-            }
-            </Table.Body>
-            </Table>
-          </Grid.Col>
-          </SiteWrapper>
+  loadData() {
+    fetch("/employee/search/all")
+      .then((response) => {
+        if (!response.ok) throw new Error("Network response was not ok");
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ data: data, loading: false });
+      })
+      .catch((err) =>
+        this.setState({ error: err.toString(), loading: false })
       );
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  render() {
+    const { data, loading, error } = this.state;
+
+    return (
+      <SiteWrapper>
+        <Page.Card title="Employee List"></Page.Card>
+        <Grid.Col md={6} lg={10} className="align-self-center">
+          {loading && <p>Loading...</p>}
+          {error && <p className="text-danger">Error: {error}</p>}
+          {!loading && !error && (
+            <Table>
+              <Table.Header>
+                <Table.ColHeader>Employee ID</Table.ColHeader>
+                <Table.ColHeader>Name</Table.ColHeader>
+                <Table.ColHeader>Email</Table.ColHeader>
+                <Table.ColHeader>Phone Number</Table.ColHeader>
+                <Table.ColHeader>Job Role</Table.ColHeader>
+                <Table.ColHeader>Job Location</Table.ColHeader>
+              </Table.Header>
+              <Table.Body>
+                {data.length > 0 ? (
+                  data.map((item, i) => (
+                    <Table.Row key={item.id || i}>
+                      <Table.Col>{item.id}</Table.Col>
+                      <Table.Col>{item.name}</Table.Col>
+                      <Table.Col>{item.email}</Table.Col>
+                      <Table.Col>{item.phone_number}</Table.Col>
+                      <Table.Col>{item.job_role}</Table.Col>
+                      <Table.Col>{item.location}</Table.Col>
+                    </Table.Row>
+                  ))
+                ) : (
+                  <Table.Row>
+                    <Table.Col colSpan={6} className="text-center">
+                      No employees found.
+                    </Table.Col>
+                  </Table.Row>
+                )}
+              </Table.Body>
+            </Table>
+          )}
+        </Grid.Col>
+      </SiteWrapper>
+    );
   }
 }
 
-export default ListEmployee
-//export default withTransaction('ListEmployee', 'component')(ListEmployee)
+export default ListEmployee;
